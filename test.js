662ce9b070121b2e97fa11ceb287811bc5a59209
Benjamin Orion Sweetnam <ben_s247@optusnet.com.au>
2017-01-13 17:54:23 +1100

    fix test

--- test.js
+++ test.js
@@ -5,7 +5,11 @@ var Bear = require('./app.js');
 test('should growl', async t => {
 	var bear = new Bear()
 	var says = bear.growl()
-  var runs = bear.run()
 	t.is(says, 'The any bear says grrr')
+})
+
+test('should run', async t => {
+  var bear = new Bear()
+  var runs = bear.run()
   t.is(runs,  'The any bear runs fast')
 })
